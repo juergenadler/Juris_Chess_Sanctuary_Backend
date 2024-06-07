3ebfa22f7d95014798e489b8a6d72ac54cf33b0e
ReaganS94 <reagansasan@gmail.com>
2024-06-07 10:33:37 +0200

    testing

--- server.js
+++ server.js
@@ -1,46 +1,23 @@
-"use strict";  // Hopefully redundant...
-
-// Global stuff...
+"use strict";
 
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const dotenv = require("dotenv");
-dotenv.config();  // Load environment variables...
-const colors = require("colors");
-const path = require("path"); 
+dotenv.config();
+require("colors");
+const path = require("path");
 
-// Our stuff...
-
-// Database
 const connectDB = require("./dbinit");
 
-// Adresses an issue on my machine that ports are being allocated
-// just before an attempt to use them, making debugging hard to handle
 const isPortInUse = require("./src/utils/checkPort");
 
-// Router imports go here...
 const userRouter = require("./src/routes/userRouter");
 const pgnRouter = require("./src/routes/pgnRouter");
 const stockfishRouter = require("./src/routes/stockfishRouter");
 
-
 async function main() {
   try {
-    // Check PORT for being allocated already.
-    // This can happen
-    // a) when another application blocks the port
-    // b) when this application is terminated without deattaching
-    // c) the debug process and node/nodemon itself collide.
-    // this leads to the debugger complaining that the port is allocated
-    // (this holds for EVERY port we want to use)
-    // while we can see that the port we want to use is allocated by node itself!
-    // This happens in VSCode as in VS 2022, more often in VS.
-    // One reason in VS 2022 is that this setting in the project file
-    // <SaveNodeJsSettingsInProjectFile>True</SaveNodeJsSettingsInProjectFile>
-    // should be present and is not (both a mystery), dependent on the history of the project
-    // newly created or generated from existing code.
-
     const PORT = process.env.PORT || 8080;
 
     const portInUse = await isPortInUse(PORT);
@@ -49,123 +26,85 @@ async function main() {
       return;
     }
 
-    // Middleware
-
     app.use(cors());
     app.use(express.json());
-    app.use(express.urlencoded({ extended: true })); // form submission (for later use)
-
-    // Spyware: Log what is going on during runtime
+    app.use(express.urlencoded({ extended: true }));
 
     app.use((req, res, next) => {
       console.log(req.path, req.method);
       next();
     });
 
-    // Connect to the database
-
     await connectDB();
     console.log("Database connected successfully.".green);
 
     const basePath = "/sanctuary";
 
-    // Welcome message
     app.get(basePath, (req, res) => {
       res.send("Welcome to my sanctuary API!");
     });
     console.log("Welcome to my sanctuary API!");
 
-    // Define your routes and middleware here
-    //
-    // Example:
-    //
-    // So the mount path is: http://localhost:PORT/<api name>/userrouter for userRouter etc.
-
-    // Serve static files from the "public" directory
     app.use(express.static(path.join(__dirname, "public")));
-    // Add a route to serve the HTML file. Test for sse in stockfishrouter
     app.get(`${basePath}/stockfishrouter/test`, (req, res) => {
-      res.sendFile(path.join(__dirname, "public", "index.html")); // Adjust the path as needed
+      res.sendFile(path.join(__dirname, "public", "index.html"));
     });
 
     // Mount the regular routers.
-    app.use(`${basePath}/userrouter`, userRouter);    
-    app.use(`${basePath}/pgnrouter`, pgnRouter);    
-    app.use(`${basePath}/stockfishrouter`, stockfishRouter);    
+    app.use(`${basePath}/userrouter`, userRouter);
+    app.use(`${basePath}/pgnrouter`, pgnRouter);
+    app.use(`${basePath}/stockfishrouter`, stockfishRouter);
 
-    // This lists all the endpoints defined so far
-    // as an array of objects.
-    // Example for such an object:
-    //{
-    //    path: '/<api_name>/userrouter/signup',
-    //    methods: ['POST'],
-    //    middlewares: ['signUpUser']
-    //}
-
-    const expressListEndpoints = require('express-list-endpoints');
-    console.log(expressListEndpoints(app));
-
-    // Start server
+    // const expressListEndpoints = require('express-list-endpoints');
+    // console.log(expressListEndpoints(app));
 
     const server = app.listen(PORT, () => {
       console.log(`Server listening on http://localhost:${PORT}`.green); // cannot read .rainbow :-P
     });
 
-    // Error handling for server startup (for sure, we checked the port being accessible already)
-    // Do we have a valid server instance here?
-    server.on('error', (err) => {
-      if (err.code === 'EADDRINUSE') {
+    server.on("error", (err) => {
+      if (err.code === "EADDRINUSE") {
         console.error(`Port ${PORT} is already in use.`);
       } else {
-        console.error('Error starting server:', err.message);
+        console.error("Error starting server:", err.message);
       }
       process.exit(1);
     });
 
-    // Otherwise: Booting or searching for the right instance of node/nodemon to kill...
-    // Handle unhandled rejections
-    process.on('unhandledRejection', (err) => {
-      console.error('Unhandled Rejection:', err);
-      // Close the server to free up the port
+    process.on("unhandledRejection", (err) => {
+      console.error("Unhandled Rejection:", err);
+
       server.close(() => {
         process.exit(1);
       });
     });
 
-    // Handle uncaught exceptions
-    process.on('uncaughtException', (err) => {
-      console.error('Uncaught Exception:', err);
-      // Close the server to free up the port
+    process.on("uncaughtException", (err) => {
+      console.error("Uncaught Exception:", err);
       server.close(() => {
         process.exit(1);
       });
     });
 
-    // Handle process termination
-    // eg. <CTRL>C in the terminal
-    process.on('SIGINT', () => {
-      console.log('SIGINT signal received. Closing server...');
+    process.on("SIGINT", () => {
+      console.log("SIGINT signal received. Closing server...");
       server.close(() => {
-        console.log('Server closed.');
-        process.exit(0); // Exit the process after closing the server, signaling no error condition
+        console.log("Server closed.");
+        process.exit(0);
       });
     });
 
-    // Handle process termination on Windows
-    // (ChatGPT) SIGBREAK is another signal commonly used for graceful termination of a process, 
-    // especially on Windows systems.While SIGINT is widely used across Unix - like 
-    // operating systems(such as Linux and macOS), Windows systems often use SIGBREAK for similar purposes.
-    process.on('SIGBREAK', () => {
-      console.log('SIGBREAK signal received. Closing server...');
+    process.on("SIGBREAK", () => {
+      console.log("SIGBREAK signal received. Closing server...");
       server.close(() => {
-        console.log('Server closed.');
-        process.exit(0); // Exit the process after closing the server, signaling no error condition
+        console.log("Server closed.");
+        process.exit(0);
       });
     });
   } catch (error) {
-    console.error('Exception occurred during server startup:', error.message);
+    console.error("Exception occurred during server startup:", error.message);
     process.exit(1);
   }
 }
 
-main().catch(err => console.error('Error in main:', err));
+main().catch((err) => console.error("Error in main:", err));
--- src/controllers/stockfishController.js
+++ src/controllers/stockfishController.js
@@ -1,28 +1,20 @@
-// Stockfishcontroller.js 
+const fs = require("fs").promises;
+const path = require("path");
 
-const mongoose = require('mongoose');
-const { Chess } = require('chess.js');
-const fs = require('fs').promises;
-const path = require('path');
-
-const PgnSchema = require('../schemas/pgnSchema');
 const {
   addPgnToDBImpl,
   getPgnByPgnIdImpl,
-  updatePgnByPgnIdImpl
-} = require('./pgnController'); // Importing the implementation functions from the controller
+  updatePgnByPgnIdImpl,
+} = require("./pgnController");
+const initLoggingEngine = require("./loggingEngine");
 
-const initLoggingEngine = require('./LoggingEngine'); // Import the LoggingEngine initialization function
-
-// Path to the Stockfish engine executable. Actually we are in the controllers folder, so we need to go up two levels to find the engines folder.
-const stockfishPath = '../../engines/stockfish/16/stockfish-windows-x86-64-sse41-popcnt.exe';
+const stockfishPath =
+  "../../engines/stockfish/16/stockfish-windows-x86-64-sse41-popcnt.exe";
 const enginePath = path.resolve(__dirname, stockfishPath);
 
-/// FEN start position for the chess board. As we are using the UCI protocol, we need to provide the initial position in Forsyth-Edwards Notation (FEN).
-// As we build up the game history from the initial position by adding a moves array we need this constant more often ;-)
-const FENstartposition = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+const FENstartposition =
+  "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-// Helper: Check if the file exists 
 const doesExist = async (filePath) => {
   try {
     await fs.access(filePath, fs.constants.F_OK);
@@ -31,7 +23,6 @@ const doesExist = async (filePath) => {
   }
 };
 
-// Helper ...and is executable
 const isExecutable = async (filePath) => {
   try {
     await fs.access(filePath, fs.constants.X_OK);
@@ -40,36 +31,41 @@ const isExecutable = async (filePath) => {
   }
 };
 
-
-// Send log updates to all connected clients via SSE. Called by the overrides in the LoggingEngine class.
 const sendLogUpdateReceived = (data) => {
   clients.forEach((client) =>
     client.res.write(`<< ${JSON.stringify(data)}\n\n`)
   );
 };
 
-// Send log updates to all connected clients via SSE. Called by the overrides in LoggingEngine class.
 const sendLogUpdateSent = (data) => {
   clients.forEach((client) =>
     client.res.write(`>> ${JSON.stringify(data)}\n\n`)
   );
 };
 
-let stockfish; // Stockfish engine instance, is being initialized in initEngine.
+let stockfish;
 
 //
-// initEngine: Initialize Stockfish
 //
 const initEngine = async (req, res) => {
+  console.log("STARTING INIT ENGINE");
   try {
     await doesExist(enginePath);
     await isExecutable(enginePath);
+    console.log("STOCKFISH INSIDE INIT");
     if (!stockfish) {
       const LoggingEngine = await initLoggingEngine();
-      stockfish = await LoggingEngine.start(enginePath, true, true, true, sendLogUpdateSent, sendLogUpdateReceived);
+      stockfish = await LoggingEngine.start(
+        enginePath,
+        true,
+        true,
+        true,
+        sendLogUpdateSent,
+        sendLogUpdateReceived
+      );
 
-      stockfish.position();      // Set the initial position
-      await stockfish.isready(); // Wait for the engine to be ready
+      stockfish.position();
+      await stockfish.isready();
       res
         .status(200)
         .json({ status: "Stockfish engine initialized and ready to use." });
@@ -83,57 +79,42 @@ const initEngine = async (req, res) => {
   }
 };
 
-
-//
-// quitEngine
-//
-// POST: /engine/quit
 const quitEngine = async (req, res) => {
   try {
     if (!stockfish) {
-      return res.status(500).json({ error: 'Stockfish engine is not initialized.' });
+      return res
+        .status(500)
+        .json({ error: "Stockfish engine is not initialized." });
     }
 
     await stockfish.quit();
-    stockfish = null; // Reset the stockfish variable, hoping we do not eat resources
+    stockfish = null;
 
-    res.status(200).json({ status: 'Stockfish terminated successfully.' });
+    res.status(200).json({ status: "Stockfish terminated successfully." });
   } catch (error) {
-    console.error('Failed to quit:', error);
-    res.status(500).json({ error: 'Failed to quit', details: error.message });
+    console.error("Failed to quit:", error);
+    res.status(500).json({ error: "Failed to quit", details: error.message });
   }
 };
 
-//
-// stopEngine: Stop the Stockfish engine calculations
-//
-// POST: /engine/stop
 const stopEngine = async (req, res) => {
   try {
     if (!stockfish) {
-      return res.status(500).json({ error: 'Stockfish engine is not initialized.' });
+      return res
+        .status(500)
+        .json({ error: "Stockfish engine is not initialized." });
     }
 
     const result = await stockfish.stop(); // How to get the best move?
-    res.status(200).json({ status: 'Stockfish calculations stopped!' });
+    res.status(200).json({ status: "Stockfish calculations stopped!" });
   } catch (error) {
-    console.error('Failed to stop calculations:', error);
-    res.status(500).json({ error: 'Failed to stop calculations', details: error.message });
+    console.error("Failed to stop calculations:", error);
+    res
+      .status(500)
+      .json({ error: "Failed to stop calculations", details: error.message });
   }
 };
 
-
-
-/**
- * MakeMove
- * POST: /engine/move/:depth
- * Handles a request to make a move with the Stockfish engine.
- * The search depth for the move can be specified as a parameter in the URL.
- * If no depth is specified, a default value of 20 is used.
- *
- * req -  The search depth should be specified as a parameter in the URL.
- * res -  The best move found by the engine will be included in the response body.
- */
 const makeMove = async (req, res) => {
   try {
     if (!stockfish) {
@@ -144,25 +125,7 @@ const makeMove = async (req, res) => {
 
     const depth = req.params.depth || 20;
 
-    stockfish.go(
-      { depth: Number(depth) },
-      //  (info) => {
-      //    if (info.depth) {
-      //      const logMessage = `depth: ${info.depth}, info: ${JSON.stringify(
-      //        info
-      //      )}`;
-      //      console.log(logMessage);
-      //      sendLogUpdateSent(logMessage); // Send this to the frontend via SSE
-      //    }
-      //  },
-      //  (result) => {
-      //    const bestpv = stockfish.pvs[0];
-      //    const logMessage = `${bestpv.score.str}/${bestpv.depth} ${result.bestmove} in ${bestpv.time}ms, ${bestpv.nodes} searched`;
-      //    console.log(logMessage);
-      //    sendLogUpdateSent(logMessage); // Send this to the frontend via SSE
-      //    res.status(200).json({ bestMove: result.bestmove });
-      //  }
-    );
+    stockfish.go({ depth: Number(depth) });
   } catch (error) {
     console.error("Failed to make move:", error);
     res
@@ -171,105 +134,42 @@ const makeMove = async (req, res) => {
   }
 };
 
-
-//
-//  analyze
-// 
-//  POST: /engine/analyze
-//  Summary: Set analysis modus that can be interrrupted by stopping the engine. So you have to stop the engine explicitly.
-//  Example: POST http://localhost:7000/stockfishrouter/engine/analyze
-//   const data = await response.json();
-//   const bestMove = data.bestmove;
-//   const ponder = data.ponder;
-//
 const analyze = async (req, res) => {
   try {
-    stockfish.go(
-      //{ depth: "infinite" }, // anything else to set the engine in analysis mode crashes the app
-      //(info) => {
-      //  if (info.depth) {
-      //    const logMessage = `depth: ${info.depth}, info: ${JSON.stringify(
-      //      info
-      //    )}`;
-      //    console.log(logMessage);
-      //    sendLogUpdateSent(logMessage); // Send this to the frontend via SSE
-      //  }
-      //},
-      //(result) => {
-      //  const bestpv = stockfish.pvs[0];
-      //  const logMessage = `${bestpv.score.str}/${bestpv.depth} ${result.bestmove} in ${bestpv.time}ms, ${bestpv.nodes} searched`;
-      //  console.log(logMessage);
-      //  sendLogUpdateSent(logMessage); // Send this to the frontend via SSE
-      //  res.status(200).json({ bestMove: result.bestmove });
-      //}
-    );
+    stockfish.go();
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-
-//
-//  Setposition
-//  POST: /engine/setposition
-//
-// Expecting FEN string and LAN array in the request body. We can handle this as JSON object,
-// also in Postman, we can use the raw JSON format to send the request.
-//
-// The moves in the "position" command of the UCI protocol are expected to be in
-// long algebraic notation(LAN). Each move consists of the starting square and the
-// ending square, optionally followed by the promotion piece(in case of pawn promotion).
-//
-// Examples:
-// e2e4: Pawn moves from e2 to e4
-// g1f3: Knight moves from g1 to f3
-// e7e8q: Pawn moves from e7 to e8 and promotes to a queen
-//
-//{
-//  "fen": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-//    "moves": ["e2e4", "e7e5", "g1f3"]
-//}
-// or
-//{
-//  "fen": "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-//    "pgn": "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6"
-//}
 const setPosition = async (req, res) => {
   try {
     if (!stockfish) {
-      return res.status(500).json({ error: 'Stockfish engine is not initialized.' });
+      return res
+        .status(500)
+        .json({ error: "Stockfish engine is not initialized." });
     }
 
-    const fen = req.body.fen || FENstartposition; // Use the FEN string from the request body, or the start position if it's not provided
-    const moves = req.body.moves || []; // Use the moves from the request body, or an empty array if they're not provided
-    await stockfish.position({ fen, moves });
-    res.status(200).json({ status: 'Position set successfully' });
+    const fen = req.body.fen || FENstartposition;
+    const moves = req.body.moves || [];
+    res.status(200).json({ status: "Position set successfully" });
   } catch (error) {
-    console.error('Failed to set position:', error);
-    res.status(500).json({ error: 'Failed to set position', details: error.message });
+    console.error("Failed to set position:", error);
+    res
+      .status(500)
+      .json({ error: "Failed to set position", details: error.message });
   }
 };
 
-
-//
-// setMoves: A variation of setPosition() that accepts moves as a URL parameter
-// POST: /engine/setmoves/:moves
-//
 const setMoves = async (req, res) => {
   try {
-    // Extract and decode the moves from the URL
     const moves = decodeURIComponent(req.params.moves);
 
-    // Split the moves by spaces
-    const components = moves.split(' ');
-
-    // Initialize variables for FEN and moves
+    const components = moves.split(" ");
     let fen = null;
     let moveList = [];
     let invalidMoves = [];
 
-    // Check if the first component is a FEN position string
-    // No validation is done here, as the chess.js library will handle this
     if (components[0].length > 10) {
       fen = components[0];
       moveList = components.slice(1);
@@ -277,41 +177,27 @@ const setMoves = async (req, res) => {
       moveList = components;
     }
 
-    // Set to the default starting position if FEN is still null
     if (!fen) {
       fen = FENstartposition;
     }
-
-    // Identify invalid moves and log them
-    // This is just a simple check to ensure that the moves are in the correct format
-    // Example moves: ["e2e4", "e7e5", "g7g8q"]
-    moveList.forEach(move => {
+    moveList.forEach((move) => {
       if (move.length < 4 || move.length > 5) {
         invalidMoves.push(move);
       }
     });
 
     if (invalidMoves.length > 0) {
-      console.log('Invalid moves:', invalidMoves);
+      console.log("Invalid moves:", invalidMoves);
     }
 
-    // Call the chess engine with the parsed FEN and all moves
     const positionResult = await stockfish.position({ fen, moves: moveList });
 
-    // Send the response (you might want to customize this based on your needs)
     res.send(`Received position: ${JSON.stringify(positionResult)}`);
   } catch (error) {
-    // Handle potential errors
     res.status(400).send(`Error processing moves: ${error.message}`);
   }
 };
 
-
-
-//
-// load game by pgn id
-// GET: /game/:id
-//
 const loadGame = async (req, res) => {
   const { id } = req.params;
   try {
@@ -319,35 +205,23 @@ const loadGame = async (req, res) => {
     if (game) {
       res.status(200).json({ game });
     } else {
-      res.status(404).json({ error: 'Game not found' });
+      res.status(404).json({ error: "Game not found" });
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-
-///
-// saveGame
-//
-// save a game as new, so we need the pg_id returned to use it
-//
-// Parameters: req.body
-//
-//
 const saveGame = async (req, res) => {
   try {
-    const newPgn = await addPgnToDBImpl(req.body); // Save the game to the database (function from pgnController.js))   
-    res.status(201).json({ status: 'Game saved', game: newPgn });
-    return newPgn; // Return the new game object, so that it can be used in the response
+    const newPgn = await addPgnToDBImpl(req.body);
+    res.status(201).json({ status: "Game saved", game: newPgn });
+    return newPgn;
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-//
-// updateGame
-// 
 const updateGame = async (req, res) => {
   const { id } = req.params;
   try {
@@ -355,23 +229,21 @@ const updateGame = async (req, res) => {
     if (game) {
       res.status(200).json({ game });
     } else {
-      res.status(404).json({ error: 'Game not found' });
+      res.status(404).json({ error: "Game not found" });
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-// SSE endpoint to handle client connections
-let clients = []; // Array to hold SSE clients
-
+let clients = [];
 const sse = (req, res) => {
   console.log("SSE Connection established");
 
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
-  res.status(200).flushHeaders(); // Send the headers and establish the SSE connection
+  res.status(200).flushHeaders();
 
   const clientId = Date.now();
   const newClient = {
@@ -387,8 +259,6 @@ const sse = (req, res) => {
   });
 };
 
-
-
 module.exports = {
   initEngine,
   quitEngine,
@@ -400,5 +270,5 @@ module.exports = {
   saveGame,
   loadGame,
   updateGame,
-  sse
+  sse,
 };
